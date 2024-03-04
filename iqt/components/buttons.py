8a228b68c32d019ca03d72e7b6d1f15417ee1f3d
e.lebedintsev <haskydancer@gmial.com>
2024-03-04 09:59:06 +0700

    update example

---
+++ iqt/components/buttons.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+from PySide6.QtWidgets import QPushButton, QWidget
+from PySide6.QtGui import QPixmap
+from PySide6.QtCore import QUrl
+from PySide6.QtNetwork import QNetworkAccessManager, QNetworkRequest
+
+from iqt.components.base import BaseWidgetObject
+
+
+class Button(
+    BaseWidgetObject,
+    signals={"items_handler": ["clicked"]}
+):
+    name: str = "default_button"
+    factory: QWidget = QPushButton
+
+    def __init__(self, text=None, **kwargs):
+        self.text = str(text) if isinstance(text, (int, float)) else text
