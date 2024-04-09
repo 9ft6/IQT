8f2342d278852f994d824958afca5b70bb9a184f
9ft6 <9ft6.dev@gmail.com>
2024-04-09 12:21:55 +0700

    data_view: add basic pagination widget, closes #3

--- iqt/components/data_view/navigation/pagination.py
+++ iqt/components/data_view/navigation/pagination.py
@@ -30,12 +30,12 @@ class Pagination(Widget):
         self.widgets = {}
 
     def update(self):
-        dataset = self.widget.dataset
-        pages = range(1, int(dataset.count() / dataset.per_page) + 1)
-        pages = list(pages) or [1]
+        pages = self.create_pages()
         self.widget.clear()
         self.widgets.clear()
+        self.widget.layout().addStretch()
         [self.add_button(p) for p in pages]
+        self.widget.layout().addStretch()
         self.ensure_current()
 
     def ensure_current(self, page=None):
@@ -43,14 +43,50 @@ class Pagination(Widget):
         for n, w in self.widgets.items():
             w.set_active(str(n) == page)
 
-        self.widgets[page].set_active(True)
+        if page in self.widgets:
+            self.widgets[page].set_active(True)
 
     def items_handler(self, sender, *args):
         page = int(sender.text())
         self.widget.dataset.set_page(page)
-        self.ensure_current(page)
+        self.ensure_current()
 
     def add_button(self, num: int):
-        widget = PageButton(str(num))
-        self.widgets[num] = widget
-        self.widget.add_widget(widget)
+        if isinstance(num, int):
+            widget = PageButton(str(num))
+            self.widgets[num] = widget
+            self.widget.add_widget(widget)
+        else:
+            self.widget.layout().addStretch()
+
+    def get_pages(self):
+        dataset = self.widget.dataset
+        current = dataset.state.page
+
+        # Getting all pages
+        pages_count = dataset.count() // dataset.per_page
+        tail = dataset.count() % dataset.per_page
+        pages = [x + 1 for x in range(0, pages_count + bool(tail))]
+
+        # Splitting by current page
+        current_index = pages.index(current) if current in pages else 1
+        left = pages[:current_index]
+        right = pages[current_index + 1:]
+
+        # Take 2 before and after current page
+        if len(left) > 2:
+            left = reversed([pages[0], ...] + left[-2:])
+        if len(right) > 2:
+            right = right[:2] + [..., pages[-1]]
+        return left, current, right
+
+    def create_pages(self):
+        left, current, right = self.get_pages()
+        result = [current]
+        for side in [left, right]:
+            for page in side:
+                if side == right:
+                    result.append(page)
+                else:
+                    result.insert(0, page)
+        return result
